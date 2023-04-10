55a118f4ab21ff655778565b4fbae47a183a069c
HukovaNastya <hukovanasta@gmail.com>
2023-04-10 14:14:53 +0300

    moved findIndex method outside function

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -1,11 +1,11 @@
 import React from 'react';
 const Carousel = ({images, currentImg, setCurrentImg}) => {
-
    const sliderContainer = {
     position: 'relative',
     height: '100%',
     top: '400px'
    }
+
    const sliderStyles = {
     width: '100%',
     height: '300px',
@@ -36,35 +36,32 @@ const Carousel = ({images, currentImg, setCurrentImg}) => {
     zIndex: 1,
     cursor: 'pointer',
    }
+   
+   let currentIndex = images.findIndex((image) => image.id === currentImg.id);
 
    const handlePrevImage = () => {
-    let currentIndex = images.findIndex((image) => image.id === currentImg.id)
-    console.log(currentIndex)
-    console.log(currentImg.id)
     if(currentIndex === 0){
-      setCurrentImg(images[images.length-1])
-    } else {
-      setCurrentImg(images[currentIndex-1])
+      setCurrentImg(images[images.length - 1])
+      } else {
+      setCurrentImg(images[currentIndex - 1])
     }
    }
 
    const handleNextImage = () => {
-    let currentIndex = images.findIndex((image) => image.id === currentImg.id)
-    console.log(currentIndex)
-    console.log(currentImg.id)
-    console.log(images.length-1)
-    if(currentIndex === images.length-1){
+    if(currentIndex === images.length - 1){
       setCurrentImg(images[0])
     } else {
-      setCurrentImg(images[currentIndex+ 1])
+      setCurrentImg(images[currentIndex + 1])
     }
    }
+
   return (
     <div style={sliderContainer}>
      <div style={leftArrowStyles} onClick={handlePrevImage}>{'<'}</div>
        <div style={rightArrowStyles} onClick={handleNextImage}>{'>'}</div>
          <div>
-          <img style={sliderStyles} src={currentImg.path}></img>
+           <img style={sliderStyles} src={currentImg.path}/>
+          {/* <img style={sliderStyles} src={currentImg.path} onClick={}></img> */}
          </div>
        </div>
   )
